cb19c972b745613a1d8e9256e737458a8193fe3d
odeaxcsh <a.babanazari.2002@gmail.com>
2024-02-27 18:43:24 +0330

    read databases from csv

--- accounts/views.py
+++ accounts/views.py
@@ -8,17 +8,42 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.views import LoginView
 
+from django.contrib.staticfiles import finders
+
+
 from django import forms 
 
+import csv
+
+
+
 class SignUpForm(UserCreationForm):
+    def __init__(self, *args, **kwargs) ->  None:
+        super().__init__(*args, **kwargs)
+        filepath = finders.find('participants.csv')
+        csvfile = csv.reader(open(filepath, 'r'), delimiter=',')
+        self.participants = dict(row for row in csvfile)
+
     class Meta:
         model = get_user_model()
         fields = ("first_name", "email", "username", "password1", "password2")
         labels={
-            "first_name": "Name",
             "username": "Student number as your username",
+            "first_name": "Name",
             "email": "AUT email address"
         }
+
+    def clean(self):
+        cleaned_data = super(UserCreationForm, self).clean()
+        username = cleaned_data.get('username')
+        email = self.participants.get(username)
+        if email is None:
+            self.add_error('username', 'Your student number is not registered. Contact website admin')
+
+        if email is not None and email != cleaned_data.get('email'):
+            self.add_error('email', 'Email is different from your email in Courses')
+        
+        return cleaned_data
 
 
 class SignUpView(CreateView):
@@ -28,5 +53,5 @@ class SignUpView(CreateView):
 
 
 class UserLoginView(LoginView):
-     template_name = "registration/login.html"
+    template_name = "registration/login.html"
 
