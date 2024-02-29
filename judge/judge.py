7b3ce117eed1137515561190469f9a16dae48bdd
odeaxcsh <a.babanazari.2002@gmail.com>
2024-02-29 19:27:19 +0330

    fix NFA

--- judge/judge.py
+++ judge/judge.py
@@ -8,8 +8,11 @@ def run(machine, tests):
     initial_state = machine['startState']
     transitions = machine['transitions']
     final_states = machine['acceptStates']
-    
+
+    states = set(transitions.keys()).union(*(state for out in transitions.values() for state in out.values()))
+
     input_symbols = set().union(*(transition.keys() for transition in transitions.values()))
+
     model_types = {
         'DFA': dfa.DFA,
         'NFA': nfa.NFA,
@@ -21,8 +24,9 @@ def run(machine, tests):
         'initial_stack_symbol': set()
     } if type == 'PDA' else {}
 
+
     model = model_types[type](
-        states=set(transitions.keys()),
+        states=states,
         input_symbols=input_symbols,
         initial_state=initial_state,
         final_states=set(final_states),
